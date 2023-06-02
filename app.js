4b58dba3abc40790df716cdc571f067c1cdf99b3
Dun Yan <ong.dy2004@gmail.com>
2023-06-02 20:06:12 +0800

    increased performance at mobile phones

--- app.js
+++ app.js
@@ -15,12 +15,36 @@ const canvas = document.querySelector('#canvas');
 const context = canvas.getContext('2d');
 let model;
 
+let performanceTime = 0.000000001;
+
+// Define the performance time values for different screen sizes
+const laptopScreenTime = 0.00000001;
+const tabletScreenTime = 0.0001;
+const smartphoneScreenTime = 10;
+
+// Function to calculate performance time based on screen size
+const calculatePerformanceTime = () => {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth <= 768) {
+        // Tablet screen size or smaller
+        performanceTime = tabletScreenTime;
+    } else if (screenWidth <= 1024) {
+        // Laptop/Computer screen size
+        performanceTime = laptopScreenTime;
+    } else {
+        // Larger screens (e.g., desktop)
+        performanceTime = smartphoneScreenTime;
+    }
+};
+
 handTrack.startVideo(video)
     .then((status) => {
         if (status) {
             navigator.getUserMedia({ video: {} }, stream => {
                 video.srcObject = stream;
-                setInterval(runDetection, 100);
+                calculatePerformanceTime();
+                setInterval(runDetection, performanceTime);
             }, err => console.log(err));
         }
     });
@@ -29,13 +53,22 @@ const runDetection = () => {
     model.detect(video)
         .then(predictions => {
             console.log(predictions);
-            model.renderPredictions(predictions, canvas, context, video);
-            if (predictions[1].label !== 'face') {
-                audio.play();
-            }            
-            if (predictions[0].label !== 'face') {
-                audio.play();
+
+            if (predictions.length > 0) {
+                model.renderPredictions(predictions, canvas, context, video);
+
+                if (predictions[1] && predictions[1].label !== 'face') {
+                    audio.play();
+                }
+            
+                if (predictions[0] && predictions[0].label !== 'face') {
+                    audio.play();
+                }
             }
+        })
+        .catch(error => {
+            // Handle any errors that occur during detection
+            console.error('Error during detection:', error);
         });
 };
 
